b610f131a10962db3ad32bc934c9d46e9a948285
breanna <breannawanjiku8@gmail.com>
2025-05-27 18:19:17 +0300

    first commit

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size  # This uses the setter
+        
+    @property
+    def size(self):
+        return self._size
+        
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+            
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
